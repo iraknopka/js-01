e72c59af82151e7f936bbdbd89ff993c73635fbd
Iryna Korman <iraknopka7@gmail.com>
2017-12-09 10:56:46 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,91 @@
+//alert('Hello World');
+//document.write('Hello World');
+console.log('Hello World');
+
+//var messageForUser
+
+//messageForUser = 'Hello,user';
+
+//document.write(messageForUser);
+
+//document.write('<br>');
+//var userAge =20;
+
+//document.write(userAge);
+
+
+//let userProfession = 'developer';
+//document.write(userProfession);
+
+//const favoriteColor = 'red';
+
+//data types
+//ES5
+let msg = 'Hello World'; //string
+
+let age =  50; //number
+let isMale = true; //bolean:true,false
+let unknown = undefined; //underfined
+let empty = null; //null
+let obj = {}; //object
+//ES2015
+
+let sym = Symbol (); //symbol
+
+
+
+
+//let userName = prompt('enter your name');
+//let grittings ='Hello' + userName;
+
+//document.write(grittings);
+
+//const UserAge = parseFloat(prompt('enter your age'));
+//const nextAge = Math.round(UserAge + 1);
+//document.write('Next year you will be'+ nextAge);
+
+
+//if(UserAge < 18) {
+    document.write('child')
+}
+    else
+{
+    document.write('adult')
+}
+
+
+//function sum (a, b, c) {
+    return  a + b + c ; 
+   
+}
+
+//const calc = sum(100, 200, 300);
+//document.write(calc);
+
+function (a, b, c) {
+    var num = input.value ;
+    document.write('input value')
+     function squared (x);
+     
+     
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
